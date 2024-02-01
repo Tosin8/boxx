0c3dbef962055ada7d5bd652e9e854c0a56f6036
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-01 18:02:35 +0100

    Updating

--- lib/screens/splash/splash_widget.dart
+++ lib/screens/splash/splash_widget.dart
@@ -8,6 +8,14 @@ class VideoBkg extends StatefulWidget {
 }
 
 class _VideoBkgState extends State<VideoBkg> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset
+  } 
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
