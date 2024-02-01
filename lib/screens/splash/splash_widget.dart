67d92979f8c470c47a85294bff0bee5541888f62
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-01 20:13:18 +0100

    Update splash_widget.dart

--- lib/screens/splash/splash_widget.dart
+++ lib/screens/splash/splash_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 class btn extends StatefulWidget {
   const btn({super.key});
@@ -18,12 +17,14 @@ class _btnState extends State<btn> {
           GestureDetector( 
             onTap: (){},
             child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(
+              width: 150, 
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
                   
                   Radius.circular(30), 
                   ),
-              
+              border: Border.all(color: Colors.white), 
               ),
               child: const Align(
                 child: Text('Log In',
@@ -31,7 +32,27 @@ class _btnState extends State<btn> {
                   fontSize: 18,
                  fontWeight: FontWeight.bold),)),
             ),
-          )
+          ), 
+          GestureDetector( 
+            onTap: (){},
+            child: Container(
+              width: 150, 
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: const BorderRadius.all(
+                  
+                  Radius.circular(30), 
+                  ),
+               
+              ),
+              child: const Align(
+                child: Text('Create Account',
+                 style: TextStyle(color: Colors.white,
+                  fontSize: 18,
+                 fontWeight: FontWeight.bold),)),
+            ),
+          ), 
         ]
 
       ),
