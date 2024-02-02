deaee4a13a6a7a087c6b859fda7bb1e02e1aa282
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-02 14:38:58 +0100

    adding form controller

--- lib/screens/form/signin/login.dart
+++ lib/screens/form/signin/login.dart
@@ -23,24 +23,27 @@ class _LoginScreenState extends State<LoginScreen> {
           color: kBkgColor, 
         ),
         
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-Padding( 
-  padding: const EdgeInsets.only(left: 0),  
-  child: IconButton(onPressed: (){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=> const SplashScreen())); 
-  }, 
-  icon: const Icon(Icons.arrow_back_ios, 
-  color: Colors.white,)),
-), 
-const SizedBox(height: 50), 
-
-Center(child: Image.asset('assets/icons/logo.png')), 
-const SizedBox(height: 8), 
-const LoginForm(), 
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30), 
+          Padding( 
+            padding: const EdgeInsets.only(left: 0),  
+            child: IconButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> const SplashScreen())); 
+            }, 
+            icon: const Icon(Icons.arrow_back_ios, 
+            color: Colors.white,)),
+          ), 
+          const SizedBox(height: 50), 
+          
+          Center(child: Image.asset('assets/icons/logo.png')), 
+          const SizedBox(height: 8), 
+          const LoginForm(), 
+            ],
+          ),
         )
       ),
     );
--- lib/screens/form/signin/login_widget.dart
+++ lib/screens/form/signin/login_widget.dart
@@ -8,14 +8,21 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey, 
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             TextFormField(
+              controller: emailController,
               keyboardType: TextInputType.emailAddress,
               textInputAction: TextInputAction.next,
             decoration: InputDecoration(
@@ -29,7 +36,7 @@ class _LoginFormState extends State<LoginForm> {
               contentPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(20),
-                borderSide: BorderSide(color: Colors.blueAccent), 
+                borderSide: const BorderSide(color: Colors.blueAccent), 
                 gapPadding: 10, ),
         
                 focusedBorder: OutlineInputBorder(
@@ -42,7 +49,7 @@ class _LoginFormState extends State<LoginForm> {
               labelStyle: TextStyle(color: Colors.white.withOpacity(0.8))
             ),
             ),
-            SizedBox(height: 10), 
+            const SizedBox(height: 20), 
              TextFormField(
               obscureText: true,
               keyboardType: TextInputType.emailAddress,
@@ -58,7 +65,7 @@ class _LoginFormState extends State<LoginForm> {
               contentPadding: const EdgeInsets.symmetric(horizontal: 42, vertical: 20),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(20),
-                borderSide: BorderSide(color: Colors.blueAccent), 
+                borderSide: const BorderSide(color: Colors.blueAccent), 
                 gapPadding: 10, ),
         
                 focusedBorder: OutlineInputBorder(
@@ -71,13 +78,14 @@ class _LoginFormState extends State<LoginForm> {
               labelStyle: TextStyle(color: Colors.white.withOpacity(0.8))
             ),
             ),
-              SizedBox(height: 10), 
+              const SizedBox(height: 20), 
               Container(
-                height: 60, width: 120,
+                height: 60, width: 180,
                 decoration: BoxDecoration(
                   color: Colors.blue, 
                   borderRadius: BorderRadius.circular(20), 
                 ),
+                child: const Align(child: Text('Log In', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),)),
               )
           ],
         ),
