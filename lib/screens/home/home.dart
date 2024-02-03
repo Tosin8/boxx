b2981d1147c15b6b64fbf3b4306e82b4e6a1d236
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-03 01:35:25 +0100

    Refactoring Search Btn

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -30,23 +30,16 @@ class _HomeScreenState extends State<HomeScreen> {
               ),), 
              // const SizedBox(height: 2), 
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: kSearchColor, 
-                  borderRadius: BorderRadius.circular(20), 
-                ),
-                child: Row(
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.magnifyingGlass,
-                       color: Colors.white.withOpacity(0.5), 
-                    ),
-                    SizedBox(width: 20), 
-                    Text('Search', style: TextStyle(color: Colors.white.withOpacity(0.5), fontSize: 18),)
-                  ],
-                ),
+                padding: const EdgeInsets.only(left: 20, right: 20), 
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: kSearchColor, 
+                    prefixIcon: Icon(FontAwesomeIcons.magnifyingGlass, color: Colors.white.withOpacity(0.6),
+                    ), 
+                    labelText: 'Search', 
+                    labelStyle: TextStyle(fontSize: 18), 
+                  ),
+                 
                 ),
               ),
             ],
