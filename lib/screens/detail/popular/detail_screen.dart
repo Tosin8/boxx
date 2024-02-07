c68f3048a19d2e6ce522c9712934f339c4b952fc
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-07 15:17:21 +0100

    Update detail_screen.dart

--- lib/screens/detail/popular/detail_screen.dart
+++ lib/screens/detail/popular/detail_screen.dart
@@ -21,8 +21,32 @@ class _DetailScreenState extends State<DetailScreen> {
             child: Column(
               children: [
                 Container(
+                  foregroundDecoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18), 
+                    gradient: LinearGradient(colors: [Colors.black.withOpacity(0.8), Colors.transparent], begin: Alignment.bottomCenter, end: Alignment.topCenter)
+                  ),
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
+                  height: MediaQuery.of(context).size.height * 0.50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(popularItems[0].imageAsset.toString()),fit: BoxFit.cover
+                    )
+                  ),
+                ), 
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Dune', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w600),), 
+                      SizedBox(height: 5,),
+                      Text('2021, Denis Villenueve', style: TextStyle(color: Colors.white54, fontSize: 20, fontWeight: FontWeight.w600
+                      ),
+                      ),
+                    ]),
+                  
+                    
+
                 )
               ],
             ),
